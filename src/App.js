59e2df5de6e6ff8e85469e7a6282eb26ed731eac
Diego Bravo <dbravo_2015@hotmail.com>
2022-06-01 20:19:16 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { HashRouter,Routes,Route } from 'react-router-dom';
+import Home from './components/Home.js'
 import './App.css';
 
 function App() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+           <h1>hola</h1>
+           
         </div>
     );
 };
